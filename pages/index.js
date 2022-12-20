bbfce06422d5aa66dc3cab0b177949e96b8e8d44
Nikita Khubaev <khubaev.n@yandex.ru>
2022-12-20 16:01:16 +0400

    nameinput workstyle input

--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,47 @@ let profileSubtitle = profile.querySelector(".profile__subtitle")
 let editButton = profile.querySelector(".profile__edit-button");
 let addButton = profile.querySelector(".profile__add-button");
 let popup = document.querySelector(".popup");
+let workstyle = popup.querySelector(".popup__input_type_workstyle");
+let user = popup.querySelector(".popup__input_type_user");
 let saveButton = popup.querySelector(".popup__save-button");
 let closeButton = popup.querySelector(".popup__close-button");
-let element = document.querySelector(".element");
-let likeButton = document.querySelector(".element__like-button");
+
+
 
 function showPopup () {
-    document.querySelector(".popup").classList.add("active");
+    user.value = profileTitle.textContent;
+    workstyle.value = profileSubtitle.textContent;
+    popup.classList.add("popup-active");
 };
 editButton.addEventListener("click", showPopup);
 
 function closePopup () {
-    document.querySelector(".popup").classList.remove("active");
+    popup.classList.remove("popup-active");
 }
 closeButton.addEventListener("click", closePopup);
 
-
-
 function addUser () {
-    document.querySelector(".popup").classList.remove("active");
-    let user = popup.querySelector(".popup__input_type_user");
+    popup.classList.remove("popup-active");
     profileTitle.textContent = `${user.value}`;
     user.value = '';
 }
 
 function addWorkstyle () {
-    document.querySelector(".popup").classList.remove("active");
-    let workstyle = popup.querySelector(".popup__input_type_workstyle");
+    popup.classList.remove("popup-active");
     profileSubtitle.textContent = `${workstyle.value}`;
     workstyle.value = '';
 }
 saveButton.addEventListener('click', addUser);
 saveButton.addEventListener('click', addWorkstyle);
+
+
+const closePopupByClickOnOverlay = function(event) {
+    if (event.target !== event.currentTarget) {
+        return;
+    } 
+    closePopup();
+}
+popup.addEventListener('click', closePopupByClickOnOverlay);
+
+
+
