dee020f965c59f24d276e0efe22399b294a089c6
Nikita Khubaev <khubaev.n@yandex.ru>
2023-01-15 14:44:10 +0400

    создана рабочая функция закрытия попапов

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,42 @@ let profile = document.querySelector('.profile');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubtitle = profile.querySelector('.profile__subtitle');
 let editButton = profile.querySelector('.profile__edit-button');
+let addButton = profile.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
+let popupAddForm = document.querySelector('.popup_add-form');
+let popupEditForm = document.querySelector('.popup_edit-form');
 let workstyle = popup.querySelector('.popup__input_type_workstyle');
 let user = popup.querySelector('.popup__input_type_user');
 let closeButton = popup.querySelector('.popup__close-button');
 let form = popup.querySelector('.popup__form');
-function showPopup() {
+let closeButtons = document.querySelectorAll('.popup__close-button');
+
+//функция открытия попапов
+function showPopupEditForm() {
     user.value = profileTitle.textContent;
     workstyle.value = profileSubtitle.textContent;
-    popup.classList.add('popup_opened');
-};
-function closePopup() {
+    popupEditForm.classList.add('popup_opened');
+    };
+    function showPopupAddForm() {
+    popupAddForm.classList.add('popup_opened');
+    };
+    editButton.addEventListener('click', showPopupEditForm);
+    addButton.addEventListener('click', showPopupAddForm);
+    
+    //функция закрытия попапов
+    function closePopup(popup) {
     popup.classList.remove('popup_opened');
-};
-function saveForm(evt) {
-    closePopup();
+    };
+    closeButtons.forEach((button) => {
+    const popup = button.closest('.popup');
+    button.addEventListener('click', () => closePopup(popup));
+    });
+    
+    //функция сохранения формы
+    function saveForm(evt) {
+    closePopup(popup);
     evt.preventDefault();
     profileTitle.textContent = user.value;
     profileSubtitle.textContent = workstyle.value;
-};
-editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', closePopup);
-form.addEventListener('submit', saveForm);
+    };
+    form.addEventListener('submit', saveForm);
